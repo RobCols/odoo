5f8e9f3f2b1b641c735b8b667786b434ba3eefe0
Vincent Baggerman <vincent.baggerman@abstractive.be>
2019-03-29 11:54:03 +0100

    App Icon on product.public.category

---
+++ product_category_public_icon/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Flex Delivery icon on ecommerce product categories',
+    'version': '12.0.1.0.0',
+    'summary': 'icon on ecommerce product categories',
+    'category': '',
+    'author': 'Abstractive BVBA',
+    'website': 'https://abstractive.be',
+    'license': 'OPL-1',
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        'views/product_public_category.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ product_category_public_icon/models/product_public_category.py
@@ -0,0 +1,7 @@
+from odoo import api, fields, models
+
+
+class ProductPublicCategory(models.Model):
+    _inherit = "product.public.category"
+
+    app_icon = fields.Binary(string='Icon')
