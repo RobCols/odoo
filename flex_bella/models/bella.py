7433262a9c475270ba4209c8041a526441428088
Vincent Baggerman <vincent.baggerman@abstractive.be>
2019-04-01 16:08:24 +0200

    Bella Triggers

--- flex_bella/models/bella.py
+++ flex_bella/models/bella.py
@@ -5,6 +5,8 @@ class BellaQuestion(models.Model):
     _name = "bella.question"
     _description = "Bella Question"
 
+    active = fields.Boolean(string='Active')
+    trigger_action_id = fields.Many2one(comodel_name="bella.action", string="Action that triggers this question")
     name = fields.Char(string="Question text", translate=True)
     answer_ids = fields.One2many(
         comodel_name="bella.answer", inverse_name="question_id", string="Answers"
@@ -15,6 +17,8 @@ class BellaAnswer(models.Model):
     _name = "bella.answer"
     _description = "Bella Answer"
 
+    active = fields.Boolean(string='Active')
+    sequence = fields.Integer(string='Sequence')
     name = fields.Char(string="Answer Text", translate=True)
     action_id = fields.Many2one(comodel_name="bella.action", string="Action to execute")
     question_id = fields.Many2one(comodel_name="bella.question", string="Question")
