74898e61cde661598a4d9150ed9caefa52808ad3
Vincent Baggerman <vincent.baggerman@abstractive.be>
2019-05-02 18:49:11 +0200

    push 3

--- push_messages/models/res_users.py
+++ push_messages/models/res_users.py
@@ -4,5 +4,5 @@ from odoo import models, fields
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    push_devices = fields.One2many(comodel_name='push.device', inverse_name='user_id', string='Push devices')
+    device_ids = fields.One2many(comodel_name='push.device', inverse_name='user_id', string='Push devices')
     
