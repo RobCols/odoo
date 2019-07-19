0683dfcfb7d8b56235c0c14c7412f36d8f4a2991
Robrecht Colson <robrecht.colson@abstractive.be>
2019-07-19 11:17:58 +0200

    Heractiveren partner form

--- flex_courier_rounds/__manifest__.py
+++ flex_courier_rounds/__manifest__.py
@@ -6,9 +6,7 @@
     "author": "Abstractive BVBA, Vincent Baggerman",
     "maintainer": "Abstractive BVBA, Vincent Baggerman",
     "depends": ["base", "web_widget_leaflet_map", "flex_routes"],
-    "data": [
-        # "views/res_partner.xml", 
-        "views/route_route.xml"],
+    "data": ["views/res_partner.xml", "views/route_route.xml"],
     "installable": True,
     "auto_install": False,
     "application": False,
